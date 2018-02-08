11e37f1406db226668d886c1489bee57624b5fa0
roy <380878382@qq.com>
2018-02-09 02:02:03 +0800

    konockout upodate

--- knockout/ko.js
+++ knockout/ko.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
         	var arr4=["A","B","C","D"];
         	var arr5=[{ name: 'Mercury', capital: null }, 
             { name: 'Earth', capital: { cityName: 'Barnsley' } }];
+            var arr6=['Japan', 'Bolivia', 'New Zealand'];
             	var bl1={
             latitude:  51.5001524,
             longitude: -0.1262362
@@ -32,10 +33,17 @@ $(document).ready(function(){
 				self.categories= ko.observableArray(arr3);
 				self.myItems= ko.observableArray(arr4);
 				self.capitals= ko.observableArray(arr5);
+				self.countries= ko.observableArray(arr6);
 				self.city= ko.observable("London");
 				self.coords= ko.observable(bl1);
 				self.twitterName=ko.observable("@example");
 				self.resultData=ko.observable();
+				self.numberofClicks=ko.observable(0);
+				self.hasCellphone=ko.observable(false);
+				self.cellphone="";
+				self.firstValue=ko.observable("hello");
+				self.secondValue="hello,again"
+				self.selectedCountry=ko.observable('wuhan');
 				self.getTweets=function(){
 					var name = self.twitterName(),
 					simulatedResults=[
@@ -45,6 +53,8 @@ $(document).ready(function(){
 					];
 					self.resultData({ retrievalDate: new Date(), topTweets: simulatedResults });
 				};
+				 self.lastInterest = ko.observable();
+        		 self.places = ko.observableArray(['London', 'Paris', 'Tokyo']);
 				self.clearResults = function(){
 					self.resultData(undefined);
 				};
@@ -55,10 +65,33 @@ $(document).ready(function(){
 				self.addPerson = function(){
 					self.person.push({name:"New at"+new Date()})
 				};
+
+				self.addNumber = function(){
+					var number = self.numberofClicks();
+					self.numberofClicks(number + 1);
+				};
+				self.setZero = function(){
+					self.numberofClicks(0);
+				};
+
+				self.logMouseover = function(data){
+					self.lastInterest(data);
+				}
 			
 			};
+
 			var  currentViewModel = new ViewModel();
+				ko.components.register('message-editor',{
+				viewModel:function(params){
+					this.text = ko.observable(params && params.initialText || '');
+				},
+				template:'Message:<input data-bind="textInput:text" />'
+				+ '(length: <span data-bind="text:text().length"  />)'
+			});
+		
 			ko.applyBindings(currentViewModel);
+
+
 
 
 		});
