aed635477ce832fb51c521e6a6248ce59ab33547
roy <380878382@qq.com>
2018-02-08 22:20:21 +0800

    konout update

--- knockout/ko.js
+++ knockout/ko.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 
-
-
 			var  ViewModel = function(){
 				var self = this;
 				var arr =  [
@@ -14,10 +12,42 @@ $(document).ready(function(){
 		        { name: 'Charles' },
 		        { name: 'Denise' }
         	];
+
+        	var arr3=[
+        		 { name: 'Fruit', items: [ 'Apple', 'Orange', 'Banana' ] },
+            { name: 'Vegetables', items: [ 'Celery', 'Corn', 'Spinach' ] }
+        	];
+
+        	var arr4=["A","B","C","D"];
+        	var arr5=[{ name: 'Mercury', capital: null }, 
+            { name: 'Earth', capital: { cityName: 'Barnsley' } }];
+            	var bl1={
+            latitude:  51.5001524,
+            longitude: -0.1262362
+        };
 				self.shounldSee = ko.observable(false);
+				self.displayMessage = ko.observable(false);
 				self.people = ko.observableArray(arr);
 				self.person= ko.observableArray(arr2);
-
+				self.categories= ko.observableArray(arr3);
+				self.myItems= ko.observableArray(arr4);
+				self.capitals= ko.observableArray(arr5);
+				self.city= ko.observable("London");
+				self.coords= ko.observable(bl1);
+				self.twitterName=ko.observable("@example");
+				self.resultData=ko.observable();
+				self.getTweets=function(){
+					var name = self.twitterName(),
+					simulatedResults=[
+						  { text: name + ' What a nice day.' },
+                { text: name + ' Building some cool apps.' },
+                { text: name + ' Just saw a famous celebrity eating lard. Yum.' }
+					];
+					self.resultData({ retrievalDate: new Date(), topTweets: simulatedResults });
+				};
+				self.clearResults = function(){
+					self.resultData(undefined);
+				};
 				self.removePerson = function(){
 					self.person.remove(this);
 				};
